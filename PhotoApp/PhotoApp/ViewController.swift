adac1345774db0477edf8ff0e0bbbb98412ff9fa
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-23 00:14:42 +0900

    Change collection view cell's size

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -41,6 +41,6 @@ extension ViewController: UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 80, height: 80)
+        return CGSize(width: 100, height: 100)
     }
 }
