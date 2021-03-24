7e0bf2b02953871e0e1af24ce6d6a4f49d1ac0df
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-24 18:09:38 +0900

    Implement UIMenuItem
    UIMenuItem will pop up when a cell in DoodleCollectionView is long pressed

--- PhotoApp/PhotoApp/SupportingFiles/PhotoCell.swift
+++ PhotoApp/PhotoApp/SupportingFiles/PhotoCell.swift
@@ -10,6 +10,10 @@ import UIKit
 class PhotoCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
     
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
--- PhotoApp/PhotoApp/View/DoodleCollectionView.swift
+++ PhotoApp/PhotoApp/View/DoodleCollectionView.swift
@@ -31,13 +31,41 @@ class DoodleCollectionView: UICollectionView {
     }
     
     @objc func handleLongPress(sender: UILongPressGestureRecognizer){
+
+        
         if sender.state == UIGestureRecognizer.State.began {
             let touchPoint = sender.location(in: self)
             print(touchPoint)
             guard let indexPath = self.indexPathForItem(at: touchPoint) else { return }
-            guard let cell = self.cellForItem(at: indexPath) else { return }
+            guard let cell = self.cellForItem(at: indexPath) as? PhotoCell else { return }
             print(cell)
+            print(cell.imageView.image)
+            cell.becomeFirstResponder()
+            
+            enableCustomMenu(targetView: cell)
         }
+    }
+    
+    func enableCustomMenu(targetView: UICollectionViewCell) {
+        let saveItem = UIMenuItem(title: "Save", action: #selector(savePhoto))
+        let dummyItem = UIMenuItem(title: "Dummy", action: #selector(dummyFunction))
+        UIMenuController.shared.menuItems = [saveItem, dummyItem]
+        // rect is not valid here
+        UIMenuController.shared.showMenu(from: targetView, rect: CGRect())
+        UIMenuController.shared.setMenuVisible(true, animated: true)
+    }
+
+    func disableCustomMenu() {
+            UIMenuController.shared.menuItems = nil
+    }
+    
+    @objc func savePhoto() {
+        print("Hello")
+//        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+    }
+    @objc func dummyFunction() {
+        print("Hello")
+//        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
     }
     
 }
