603f57ab5d85583c60daee5ad489c079ffd1f287
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-24 15:21:23 +0900

    Implement JSON decoder

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -6,21 +6,24 @@
 //
 
 import UIKit
-struct Doodle: Codable {
-    var title: String
-    var image: String
-    var date: String
-    
-}
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlPath = Bundle.main.url(forResource: "doodle", withExtension: "json")
-        print(urlPath!)
-        let data = try! Data(contentsOf: urlPath!)
-        let arr = try! JSONDecoder().decode([Doodle].self, from: data)
-        print(arr)
+        parseDoodle()
+    }
+    
+    func parseDoodle() {
+        guard let urlPath = Bundle.main.url(forResource: "doodle", withExtension: "json") else { return }
+        do{
+            let data = try Data(contentsOf: urlPath)
+            let decodedData = try JSONDecoder().decode([Doodle].self, from: data)
+            print(decodedData)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
+
