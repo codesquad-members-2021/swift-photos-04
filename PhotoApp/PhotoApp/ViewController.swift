85e14953f25408c7bdb253add4d43ae516034589
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-24 15:08:54 +0900

    Add doodle file
    Added doodle json file
    Tried temporary decoder

--- PhotoApp/PhotoApp/ViewController.swift
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -6,10 +6,21 @@
 //
 
 import UIKit
-
+struct Doodle: Codable {
+    var title: String
+    var image: String
+    var date: String
+    
+}
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlPath = Bundle.main.url(forResource: "doodle", withExtension: "json")
+        print(urlPath!)
+        let data = try! Data(contentsOf: urlPath!)
+        let arr = try! JSONDecoder().decode([Doodle].self, from: data)
+        print(arr)
     }
 }
