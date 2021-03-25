6b6b2aea683bd61a47b2812415d079ee14cbda3b
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-25 14:48:25 +0900

    Present doodleViewController when plus bar button pressed

--- PhotoApp/PhotoApp/Controller/DoodleViewController.swift
+++ PhotoApp/PhotoApp/Controller/DoodleViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class DoodleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
